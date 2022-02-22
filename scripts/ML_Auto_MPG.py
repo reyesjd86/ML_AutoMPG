f234ab0a59d0f5fe1de3ffc872420ecf6fe10eb2
Jason Reyes <reyesjd86@gmail.com>
2022-02-21 21:35:14 -0500

    add requirements, started on script, deleted untitled.py

--- scripts/ML_Auto_MPG.py
+++ scripts/ML_Auto_MPG.py
@@ -4,4 +4,52 @@ Created on Mon Feb 21 21:11:36 2022
 
 @author: jreye
 """
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#%%
+path = r'C:\Users\jreye\Documents\Projects\ML_AutoMPG\scripts'
+os.chdir(path)
+# defining the column names
+cols = ['MPG','Cylinders','Displacement','Horsepower','Weight',
+                'Acceleration', 'Model Year', 'Origin']
+# reading the .data file using pandas
+df = pd.read_csv('../data/auto-mpg.data', names=cols, na_values = "?",
+                comment = '\t',
+                sep= " ",
+                skipinitialspace=True)
+#making a copy of the dataframe
+data = df.copy()
+#%%
+##checking for all the null values
+print(data.isnull().sum())
+#%%
+##summary statistics of quantitative variables
+data.describe()
+
+##looking at horsepower box plot
+sns.boxplot(x=data['Horsepower'])
+#%%
+##imputing the values with median
+median = data['Horsepower'].median()
+data['Horsepower'] = data['Horsepower'].fillna(median)
+data.info()
+#%%
+##category distribution
+
+data["Cylinders"].value_counts() / len(data)
+data['Origin'].value_counts()
+print(data.head())
+#%%
+##pairplots to get an intuition of potential correlations
+sns.pairplot(data[["MPG", "Cylinders", "Displacement", "Weight", "Horsepower"]], diag_kind="kde")
+#%%
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(data, data["Cylinders"]):
+    strat_train_set = data.loc[train_index]
+    strat_test_set = data.loc[test_index]
 
--- scripts/untitled0.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 21 21:08:07 2022
-
-@author: jreye
-"""
-
