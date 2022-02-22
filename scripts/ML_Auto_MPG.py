2e69907b340954ba946a7c918be7a0890e96d0de
Jason Reyes <reyesjd86@gmail.com>
2022-02-21 21:14:54 -0500

    created setup

---
+++ scripts/ML_Auto_MPG.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 21:11:36 2022
+
+@author: jreye
+"""
+
---
+++ scripts/untitled0.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 21:08:07 2022
+
+@author: jreye
+"""
+
